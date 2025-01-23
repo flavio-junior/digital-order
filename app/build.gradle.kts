b08003c8e31fe308a69d494cc4e0742bd6833b8f
Flávio Júnior <59364674+flavio-junior@users.noreply.github.com>
2025-01-23 10:21:52 -0300

    fix: add version of code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "br.com.digital.order"
         minSdk = 23
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
